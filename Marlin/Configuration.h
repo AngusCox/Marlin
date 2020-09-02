6c2e2ef9cee1a4656e1a3daae6ad5960ca15a317
AngusCox <54340236+AngusCox@users.noreply.github.com>
2020-09-02 11:36:31 -0300

    Final adjustments
    Sentido Motores
    Offset Probe
    Sensibilidad Homing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -990,7 +990,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -33.5, -3.5, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -35.5, -3.5, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1090,14 +1090,14 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false
-#define INVERT_Y_DIR false
-#define INVERT_Z_DIR true
+#define INVERT_X_DIR true
+#define INVERT_Y_DIR true
+#define INVERT_Z_DIR false
 
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
